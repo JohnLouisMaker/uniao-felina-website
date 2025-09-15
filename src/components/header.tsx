2a5e0bc788c0bb6e86390c13c19c24f8a68a82cf
JohnLouisMaker <jluisilvenancio@gmail.com>
2025-09-14 21:38:20 -0300

    feat: Header begins

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,14 @@
 export default function Header() {
-    return (
-        <header>
-            <h1 className="bg-blue-500">Header</h1>
-        </header>
-    )
+  return (
+    <header className="flex justify-center bg-yellow-300">
+      <nav>
+        <ul className="flex p-3 gap-6">
+          <li>Quem Sómos</li>
+          <li>Nossa História</li>
+          <li>Seja Voluntário</li>
+          <li>Contatos</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
